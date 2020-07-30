5888887c613068c3be95d5d11285e54171eacebf
Jason Imison <nosami@users.noreply.github.com>
2020-07-30 18:19:36 +0100

    Update vncserver.ps1

--- vncserver.ps1
+++ vncserver.ps1
@@ -1,3 +1,3 @@
 choco install -y ultravnc 
 cp ultravnc.ini "C:\Program Files\uvnc bvba\UltraVNC"
-Start-Job -ScriptBlock { & 'C:\Program Files\uvnc bvba\UltraVNC\winvnc -run' }
+Start-Job -ScriptBlock { & 'C:\Program Files\uvnc bvba\UltraVNC\winvnc' -run }
