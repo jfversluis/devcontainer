84af663d5a68a9e240313545a774594186c2c422
Jason Imison <jaimison@microsoft.com>
2020-08-01 10:47:19 +0100

    change RDP port

--- vncserver.ps1
+++ vncserver.ps1
@@ -1,4 +1,5 @@
 choco install -y ultravnc 
 cp ultravnc.ini "C:\Program Files\uvnc bvba\UltraVNC"
 start-process "C:\Program Files\uvnc bvba\UltraVNC\winvnc" -ArgumentList "-run"
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name PortNumber -Value 3390
 
