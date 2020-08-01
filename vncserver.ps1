1785633c67f44de428a5db98f21a4ce1f66033df
Jason Imison <jaimison@microsoft.com>
2020-08-01 12:29:10 +0100

    restart RDP service

--- vncserver.ps1
+++ vncserver.ps1
@@ -2,4 +2,7 @@ choco install -y ultravnc
 cp ultravnc.ini "C:\Program Files\uvnc bvba\UltraVNC"
 start-process "C:\Program Files\uvnc bvba\UltraVNC\winvnc" -ArgumentList "-run"
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name PortNumber -Value 3390
-
+Get-Service -Name 'Remote Desktop Services UserMode Port Redirector' | Stop-Service -Force -Verbose
+Get-Service -Name 'TermService' | Stop-Service -Force -Verbose
+Get-Service -Name 'TermService' | Start-Service -Verbose
+Get-Service -Name 'Remote Desktop Services UserMode Port Redirector' | Start-Service -Verbose
