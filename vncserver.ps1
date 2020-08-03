510deff0e4e6e56c4b7b4bca14d0823b555502d7
Jason Imison <jaimison@microsoft.com>
2020-08-03 18:55:38 +0100

    restart rdp

--- vncserver.ps1
+++ vncserver.ps1
@@ -4,10 +4,7 @@ start-process "C:\Program Files\uvnc bvba\UltraVNC\winvnc" -ArgumentList "-run"
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name PortNumber -Value 3390
 
 $SecurePassword = ConvertTo-SecureString "hello" -AsPlainText -Force
-$UserAccount = Get-LocalUser -Name "testuser"
+$UserAccount = Get-LocalUser -Name "vsonline"
 $UserAccount | Set-LocalUser -Password $SecurePassword
 
-Get-Service -Name 'Remote Desktop Services UserMode Port Redirector' | Stop-Service -Force -Verbose
-Get-Service -Name 'TermService' | Stop-Service -Force -Verbose
-Get-Service -Name 'TermService' | Start-Service -Verbose
-Get-Service -Name 'Remote Desktop Services UserMode Port Redirector' | Start-Service -Verbose
+Get-Service -Name 'Remote Desktop Services' | Restart-Service -Force -Verbose
