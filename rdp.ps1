ecdfff325905cda334a9a312077bc6996199e0ef
Gerald Versluis <gerald@verslu.is>
2021-02-24 17:00:27 +0100

    Update rdp.ps1

--- rdp.ps1
+++ rdp.ps1
@@ -4,4 +4,6 @@ $SecurePassword = ConvertTo-SecureString "hello" -AsPlainText -Force
 $UserAccount = Get-LocalUser -Name "vsonline"
 $UserAccount | Set-LocalUser -Password $SecurePassword
 
-Get-Service -Name 'Remote Desktop Services' | Restart-Service -Force -Verbose
+# Get-Service -Name 'Remote Desktop Services' | Restart-Service -Force -Verbose
+$id = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'TermService'" | Select-Object -ExpandProperty ProcessId
+Stop-Process $id -Force
