0cb23f26af3cb6aea93e11b6918cc304666b4325
Gerald Versluis <gerald@verslu.is>
2021-02-24 16:43:47 +0100

    Update rdp.ps1

--- rdp.ps1
+++ rdp.ps1
@@ -1,3 +1,5 @@
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name PortNumber -Value 3390
+
 $SecurePassword = ConvertTo-SecureString "hello" -AsPlainText -Force
 $UserAccount = Get-LocalUser -Name "vsonline"
 $UserAccount | Set-LocalUser -Password $SecurePassword
